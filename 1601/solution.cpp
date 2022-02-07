2f88bec7d9646d9088704430fe381c183a4990a2
mkmark <mark@mkmark.net>
2022-02-07 19:15:24 +0800

    1601

---
+++ 1601/solution.cpp
@@ -0,0 +1,39 @@
+#include <vector>       // std::vector
+#include <iostream>     // std::iostream
+#include <numeric>      // std::accumulate
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        vector<int> inflow(n,0);
+        return dfs(requests, 0, inflow);
+    }
+    int dfs(vector<vector<int>>& requests, int i, vector<int> &inflow){
+        if (i>=requests.size()){
+            if (is_all_0(inflow)){
+                return 0;
+            } else {
+                return INT32_MIN;
+            }
+        }
+        vector<int> inflow_with_i = inflow;
+        --inflow_with_i[requests[i][0]];
+        ++inflow_with_i[requests[i][1]];
+        return max(
+            dfs(requests, i+1, inflow),
+            1+dfs(requests, i+1, inflow_with_i)
+        );
+    }
+    bool is_all_0(vector<int>& arr){
+        for (auto i : arr){
+            if (i) return false;
+        }
+        return true;
+    }
+};
+
+// https://stackoverflow.com/questions/31162367/significance-of-ios-basesync-with-stdiofalse-cin-tienull
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
