d12ea2d4cc63ac382e6229334d53d1262eebf22a
mkmark <mark@mkmark.net>
2022-02-09 03:49:12 +0800

    2002

---
+++ 2002/solution-2.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/maximum-product-of-the-length-of-two-palindromic-subsequences/discuss/1458289/Mask-DP
+
+#include <vector>       // std::vector
+#include <iostream>     // std::iostream
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     *  @brief if masked string is a palindrome, return its length; else return 0.
+     */
+    int palindrome_length(string &s, int mask) {
+        int p1 = 0, p2 = s.size(), res = 0;
+        while (p1 <= p2) {
+            if ((mask & (1 << p1)) == 0)
+                ++p1;
+            else if ((mask & (1 << p2)) == 0)
+                --p2;
+            else if (s[p1] != s[p2])
+                return 0;
+            else
+                res += 1 + (p1++ != p2--);
+        }
+        return res;
+    }
+    int maxProduct(string s) {
+        int dp[4096];
+        int res = 0;
+        int mask = (1 << s.size()) - 1;
+        // get all palindrome_length
+        for (int m = 1; m <= mask; ++m){
+            dp[m] = palindrome_length(s, m);
+        }
+        for (int m1 = mask; m1; --m1){
+            // pruning to skip masks with no potential to surpass the current maximum
+            if (dp[m1] * (s.size() - dp[m1]) > res){
+                // check only "available" bits (mask ^ m1)
+                for(int m2 = mask ^ m1; m2; m2 = (m2 - 1) & (mask ^ m1)){
+                    res = max(res, dp[m1] * dp[m2]);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// https://stackoverflow.com/questions/31162367/significance-of-ios-basesync-with-stdiofalse-cin-tienull
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
---
+++ 2002/solution.cpp
@@ -0,0 +1,60 @@
+#include <vector>       // std::vector
+#include <iostream>     // std::iostream
+#include "math.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(string s) {
+        int n = s.length();
+        int max_product = -1;
+
+        for (int i=0;i<pow(2,n-1);++i){
+            string s1 = "";
+            string s2 = "";
+            int x = i;
+            int p = 0;
+            while(x != 0){
+                if (x%2){
+                    s1.push_back(s.at(p++));
+                } else {
+                    s2.push_back(s.at(p++));
+                }
+                x >>= 1;
+            }
+            for (p;p<n;++p){
+                s2.push_back(s.at(p));
+            }
+            max_product = max(
+                max_product,
+                max_sub_palindrome_length(s1)*max_sub_palindrome_length(s2)
+            );
+        }
+        return max_product;
+    }
+
+    int max_sub_palindrome_length(string &s){
+        int n = s.length();
+        if (n==0){
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i=n-1; i>=0; --i){
+            dp[i][i] = 1;
+            for (int j=i+1; j<n; ++j){
+                if (s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
+    }   
+};
+
+// https://stackoverflow.com/questions/31162367/significance-of-ios-basesync-with-stdiofalse-cin-tienull
+static bool     _foo = ios::sync_with_stdio(false);
+static ostream* _bar = cin.tie(NULL);
